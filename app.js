a0a95ea6149026c940addb9136251fbf21f94510
joaoenrique13 <jebsantosalves@gmail.com>
2024-04-16 14:23:03 -0300

    tabelas do banco

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const handlebars = require('express-handlebars').engine
 const bodyParser = require('body-parser')
 const port = 8081
+const tables = require('./models/tables')
 
 
 app.engine('handlebars', handlebars( {defaultLayout: 'main'}));
@@ -12,79 +13,33 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-// Create
+// index
 app.get('/', function(req, res){
     res.render('index')
 })
 
 // Read
-app.get('/consultar', async (req, res) =>{
-    const agendamentos = await post.findAll();
-    const params = req.query;
-
-      res.render('consultar', {
-        agendamentos: agendamentos.map((post) => post.get({ plain: true })), 
-        params
-    });
+app.get('/read', async (req, res) =>{
 })
 
-// Update
-app.get('/atualizar/:id', async (req, res) =>{
-    const id = req.params.id;
-    const agendamento = await post.findByPk(id);
+// View Create
+app.get('/create', async (req, res) =>{
+})
 
-    if(agendamento == null){
-        res.redirect('/')
-    }
-    const agendamentoJSON = agendamento.toJSON();
-
-    res.render('atualizar', { agendamento: agendamentoJSON });
+// View edit
+app.get('/edit/:id', async (req, res) =>{
 })
 
 // Delete
-app.post('/apagar/:id', async (req, res) =>{
-    const id = req.params.id;
-
-    await post.destroy({
-        where: {
-          id: id,
-        },
-      });
-
-    res.redirect('/consultar?sucess=Agendamento excluido com sucesso');
+app.post('/delete/:id', async (req, res) =>{
 })
 
-
-app.post('/cadastrar', function(req, res){
-    post.create({
-        nome: req.body.nome,
-        telefone: req.body.telefone,
-        origem: req.body.origem,
-        data_contato: req.body.data_contato,
-        observacao: req.body.observacao,
-    }).then(function(){
-        res.redirect('/consultar?sucess=Agendamento criado com sucesso');
-    }).catch(function(error){
-        res.redirect('/consultar?danger=Erro ao criar agendamento');
-    })
+app.post('/save', function(req, res){
 })
 
-app.post('/atualizar', async (req, res) =>{
-    const agendamento = await post.findByPk(req.body.id);
+app.post('/update', async (req, res) =>{
 
-    agendamento.update({
-        nome: req.body.nome,
-        telefone: req.body.telefone,
-        origem: req.body.origem,
-        data_contato: req.body.data_contato,
-        observacao: req.body.observacao,
-    }).then(function(){
-        res.redirect('/consultar?sucess=Agendamento atualizado com sucesso');
-    }).catch(function(error){
-        res.redirect('/consultar?danger=Erro ao atualizar agendamento');
-    })
 })
-
 
 
 app.listen(port, function(){
