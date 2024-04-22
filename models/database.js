5d59710b9d4efc506989a60f9376c634c9d70f49
joaoenrique <jebsantosalves@gmail.com>
2024-04-22 13:21:33 -0300

    correcao mysql

--- models/database.js
+++ models/database.js
@@ -1,15 +1,8 @@
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize(process.env.POSTGRES_DATABASE, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
-    host: process.env.POSTGRES_HOST,
-    dialect: 'postgres',
-    logging: false, // Opcional: desativa o log de consultas SQL
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false // Adicione isso se você estiver tendo problemas com a verificação do certificado SSL
-        }
-    }
-});
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mariadb'
+})
 
 module.exports = {
     Sequelize: Sequelize,
