be57f6eae9aaa06704344aea90ccaff522aeb43d
joaoenrique <jebsantosalves@gmail.com>
2024-04-22 16:36:46 -0300

    correcao de conexao com banco

--- models/database.js
+++ models/database.js
@@ -1,15 +1,9 @@
-const { Sequelize, DataTypes } = require('sequelize');
-require('dotenv').config();
+const { Pool } = require('pg')
+require('dotenv').config()
 
-const sequelize = new Sequelize('postgres://default:xVqFA8PYp1ro@ep-lingering-tree-465388-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require', {
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false 
-    }
-  }
-});
+const pool = new Pool({
+  connectionString: "postgres://default:xVqFA8PYp1ro@ep-lingering-tree-465388-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
+})
 
 module.exports = {
     Sequelize: Sequelize,
