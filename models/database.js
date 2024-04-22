6507b5e4cd741a7efb7a3971d60ae797adb7431d
joaoenrique <jebsantosalves@gmail.com>
2024-04-22 16:12:54 -0300

    correcao mysql

--- models/database.js
+++ models/database.js
@@ -1,10 +1,16 @@
-const Sequelize = require('sequelize')
-const sequelize = new Sequelize('rezendes_moda', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql'
-})
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.POSTGRES_URL, {
+  logging: true, // opcional, para ver as consultas SQL geradas no console
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // opcional, dependendo das configurações do seu banco de dados
+    }
+  }
+});
 
 module.exports = {
-    Sequelize: Sequelize,
-    sequelize: sequelize
-}
+  Sequelize: Sequelize,
+  sequelize: sequelize
+};
