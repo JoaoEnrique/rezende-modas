942199ab6f1e01883a5b2f691176a76df91d5fc9
joaoenrique <jebsantosalves@gmail.com>
2024-04-22 17:54:08 -0300

    correcao dialect

--- models/database.js
+++ models/database.js
@@ -1,9 +1,8 @@
-require('dotenv').config();
 const Sequelize = require('sequelize')
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
-});
+    dialect: 'mariadb'
+})
 
 module.exports = {
     Sequelize: Sequelize,
