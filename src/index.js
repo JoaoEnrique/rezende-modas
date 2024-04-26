af769415ea933f8c3d094deaf85dc70372e3f8b5
joaoenrique <jebsantosalves@gmail.com>
2024-04-26 10:57:10 -0300

    interface

--- src/index.js
+++ src/index.js
@@ -13,7 +13,9 @@ const path = require('path');
 
 app.engine('handlebars', exphbs.engine({
     defaultLayout: "main",
-    layoutsDir: path.join(__dirname, "views", "layouts")
+    layoutsDir: path.join(__dirname, "views", "layouts"),
+    partialsDir: path.join(__dirname, "views", "layouts") 
+
 }));
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, "views"));
--- src/routes/views.js
+++ src/routes/views.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // ROTAS
+router.use(express.static('src'));
 
 // index
 router.get('/', function(req, res){
@@ -30,6 +31,58 @@ router.post('/save', function(req, res){
 
 router.post('/update', async (req, res) =>{
 
+})
+
+
+// Funcionarios
+router.get('/login', function(req, res){
+    res.render('login')
+})
+
+// Funcionarios
+router.get('/employees', function(req, res){
+    res.render('employees/index')
+})
+
+router.get('/employees/list', function(req, res){
+    res.render('employees/list')
+})
+
+router.get('/employees/register', function(req, res){
+    res.render('employees/register')
+})
+
+router.get('/employees/edit', function(req, res){
+    res.render('employees/edit')
+})
+
+
+// Produtos
+router.get('/products', function(req, res){
+    res.render('products/index')
+})
+
+router.get('/products/list', function(req, res){
+    res.render('products/list')
+})
+
+router.get('/products/register', function(req, res){
+    res.render('products/register')
+})
+
+router.get('/products/edit/:id', function(req, res){
+    const id = req.params.id;
+    res.render('products/edit', {id: id})
+})
+
+
+// Catalogo
+router.get('/catalogs/masculine', function(req, res){
+    res.render('catalogs/masculine')
+})
+
+router.get('/catalogs/feminine', function(req, res){
+    res.render('catalogs/feminine')
 })
 
 module.exports = router
